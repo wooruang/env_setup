35520d6d10f1b9198fd01a01a04dbbf21018f714
wooruang <wooruang@gmail.com>
2018-10-12 10:53:18 +0900

    Add vim-plug install code in install script

--- install.sh
+++ install.sh
@@ -10,9 +10,21 @@ function getScriptDirectory {
 
 SCRIPT_DIR=`getScriptDirectory`
 
+# Install git
 GITCONFIG=$HOME/.gitconfig
 ADD_GITCONFIG=$SCRIPT_DIR/gitconfig
 
 echo -e "[include]"               >> $GITCONFIG 
 echo -e "\tpath = $ADD_GITCONFIG" >> $GITCONFIG 
+
+# Install vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+VIMRC=$HOME/.vimrc
+ADD_VIMRC=$SCRIPT_DIR/.vimrc
+
+echo -e "source $ADD_VIMRC" >> $VIMRC
+
+echo "Put :PlugInstall in vim!"
 
